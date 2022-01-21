4781ac1f37c49f6343810d7f32b0623e948a32dd
emirkovacevic <emir.kovacevic@jjryan.com.au>
2022-01-21 15:26:23 +1000

    first commit

---
+++ ContactBook/ContactBook/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ContactBook
+{
+    public partial class MainPage : ContentPage
+    {
+        public List<Contact> Contacts { get; private set; }
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            Contacts = new List<Contact>();
+            Contacts.Add(new Contact { Name = "Emir", ImageUrl = "https://static.toiimg.com/thumb/resizemode-4,msid-76729750,imgsize-249247,width-720/76729750.jpg", Status = "My status" });
+            Contacts.Add(new Contact { Name = "Tom", ImageUrl = "https://static.toiimg.com/thumb/resizemode-4,msid-76729750,imgsize-249247,width-720/76729750.jpg" });
+            Contacts.Add(new Contact { Name = "Jess", ImageUrl = "https://static.toiimg.com/thumb/resizemode-4,msid-76729750,imgsize-249247,width-720/76729750.jpg" });
+
+            listView.ItemsSource = Contacts;
+        }
+
+
+        async private void listView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            //this prevents infinite loop when button is pressed
+            if(e.SelectedItem == null)
+            {
+                return;
+            }
+
+            var contact = e.SelectedItem as Contact;
+            await Navigation.PushAsync(new ContactDetailsPage(contact));
+
+            listView.SelectedItem = null;
+        }
+
+        async private void go_to_tabbed_page_Pressed(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new TabsShowcasePage());
+        }
+
+        async private void go_to_grid_page_Pressed(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new GridShowcasePage());
+        }
+
+        
+    }
+}
---
+++ ContactBook/ContactBook/Views/ListRow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ContactBook
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListRow : ViewCell
+    {   
+        //Example of bindableProperty
+        public static readonly BindableProperty NameProperty =
+            BindableProperty.Create("Label", typeof(string), typeof(ListRow), null);
+        public string Name
+        {
+            get { return (String)GetValue(NameProperty); }
+            set { SetValue(NameProperty, value); }
+        }
+
+        public static readonly BindableProperty StatusProperty =
+            BindableProperty.Create("Label", typeof(string), typeof(ListRow), null);
+        public string Status
+        {
+            get { return (String)GetValue(StatusProperty); }
+            set { SetValue(StatusProperty, value); }
+        }
+
+        public static readonly BindableProperty ImageProperty =
+            BindableProperty.Create("Image", typeof(string), typeof(ListRow), null);
+        public string ImageUrl
+        {
+            get { return (String)GetValue(ImageProperty); }
+            set { SetValue(ImageProperty, value); }
+        }
+        
+        public ListRow()
+        {
+            InitializeComponent();
+
+            BindingContext = this;
+        }
+
+        async private void Call_Pressed(object sender, EventArgs e)
+        {
+            //Example of passing dynamic paramete (Command parameter) as event argument 
+            string caller = ((Button)sender).CommandParameter as string;
+
+            //Example how to reach components that are out of reach
+            await App.Current.MainPage.DisplayAlert("Calling", caller, "OK");
+        }
+    }
+}
